19ea15921200d8923c966e68c239c5786d34189a
Bongjae <kbjkim@naver.com>
2022-08-01 19:56:27 +0900

    20220801업로드

--- pro27/pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
+++ pro27/pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
@@ -35,11 +35,11 @@ public class MemberControllerImpl   implements MemberController {
 	@Override
 	@RequestMapping(value="/member/listMembers.do" ,method = RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
-//		String viewName = (String)request.getAttribute("viewName");
-		//System.out.println("viewName: " +viewName);
-		logger.info("info 레벨 : viewName = "+ viewName);
-		logger.debug("debug 레벨 : viewName = "+ viewName);
+//		String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
+//		System.out.println("viewName: " +viewName);
+//		logger.info("info 레벨 : viewName = "+ viewName);
+//		logger.debug("debug 레벨 : viewName = "+ viewName);
 		List membersList = memberService.listMembers();
 		ModelAndView mav = new ModelAndView(viewName);
 		mav.addObject("membersList", membersList);
@@ -111,45 +111,12 @@ public class MemberControllerImpl   implements MemberController {
 	private ModelAndView form(@RequestParam(value= "result", required=false) String result,
 						       HttpServletRequest request, 
 						       HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
-		//String viewName = (String)request.getAttribute("viewName");
+//		String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("result",result);
 		mav.setViewName(viewName);
 		return mav;
 	}
 	
-
-	private String getViewName(HttpServletRequest request) throws Exception {
-		String contextPath = request.getContextPath();
-		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
-		if (uri == null || uri.trim().equals("")) {
-			uri = request.getRequestURI();
-		}
-
-		int begin = 0;
-		if (!((contextPath == null) || ("".equals(contextPath)))) {
-			begin = contextPath.length();
-		}
-
-		int end;
-		if (uri.indexOf(";") != -1) {
-			end = uri.indexOf(";");
-		} else if (uri.indexOf("?") != -1) {
-			end = uri.indexOf("?");
-		} else {
-			end = uri.length();
-		}
-
-		String viewName = uri.substring(begin, end);
-		if (viewName.indexOf(".") != -1) {
-			viewName = viewName.substring(0, viewName.lastIndexOf("."));
-		}
-		if (viewName.lastIndexOf("/") != -1) {
-			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
-		}
-		return viewName;
-	}
-
-
 }
--- pro30/src/main/java/com/myspring/pro30/member/controller/MemberControllerImpl.java
+++ pro30/src/main/java/com/myspring/pro30/member/controller/MemberControllerImpl.java
@@ -30,16 +30,24 @@ public class MemberControllerImpl   implements MemberController {
 	@Autowired
 	private MemberService memberService;
 	@Autowired
-	MemberVO memberVO ;
+	private MemberVO memberVO ;
+	
+	@RequestMapping(value = { "/","/main.do"}, method = RequestMethod.GET)
+    private ModelAndView main(HttpServletRequest request, HttpServletResponse response) {
+        String viewName = (String)request.getAttribute("viewName");
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName(viewName);
+        return mav;
+    }
 	
 	@Override
-	@RequestMapping(value={"/","/main.do"} ,method = RequestMethod.GET)
+	@RequestMapping(value="/member/listMembers.do" ,method = RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
-//		String viewName = (String)request.getAttribute("viewName");
+		//String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
 		//System.out.println("viewName: " +viewName);
-		logger.info("info 레벨 : viewName = "+ viewName);
-		logger.debug("debug 레벨 : viewName = "+ viewName);
+		logger.info("viewName: "+ viewName);
+		logger.debug("viewName: "+ viewName);
 		List membersList = memberService.listMembers();
 		ModelAndView mav = new ModelAndView(viewName);
 		mav.addObject("membersList", membersList);
@@ -66,16 +74,16 @@ public class MemberControllerImpl   implements MemberController {
 		ModelAndView mav = new ModelAndView("redirect:/member/listMembers.do");
 		return mav;
 	}
-	/*
+	
 	@RequestMapping(value = { "/member/loginForm.do", "/member/memberForm.do" }, method =  RequestMethod.GET)
-	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
+//	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
 	public ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
+		//String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName(viewName);
 		return mav;
-	}*/
-
+	}
 	
 	@Override
 	@RequestMapping(value = "/member/login.do", method = RequestMethod.POST)
@@ -111,8 +119,8 @@ public class MemberControllerImpl   implements MemberController {
 	private ModelAndView form(@RequestParam(value= "result", required=false) String result,
 						       HttpServletRequest request, 
 						       HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
-		//String viewName = (String)request.getAttribute("viewName");
+		//String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("result",result);
 		mav.setViewName(viewName);
@@ -120,36 +128,36 @@ public class MemberControllerImpl   implements MemberController {
 	}
 	
 
-	private String getViewName(HttpServletRequest request) throws Exception {
-		String contextPath = request.getContextPath();
-		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
-		if (uri == null || uri.trim().equals("")) {
-			uri = request.getRequestURI();
-		}
-
-		int begin = 0;
-		if (!((contextPath == null) || ("".equals(contextPath)))) {
-			begin = contextPath.length();
-		}
-
-		int end;
-		if (uri.indexOf(";") != -1) {
-			end = uri.indexOf(";");
-		} else if (uri.indexOf("?") != -1) {
-			end = uri.indexOf("?");
-		} else {
-			end = uri.length();
-		}
-
-		String viewName = uri.substring(begin, end);
-		if (viewName.indexOf(".") != -1) {
-			viewName = viewName.substring(0, viewName.lastIndexOf("."));
-		}
-		if (viewName.lastIndexOf("/") != -1) {
-			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
-		}
-		return viewName;
-	}
+//	private String getViewName(HttpServletRequest request) throws Exception {
+//		String contextPath = request.getContextPath();
+//		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+//		if (uri == null || uri.trim().equals("")) {
+//			uri = request.getRequestURI();
+//		}
+//
+//		int begin = 0;
+//		if (!((contextPath == null) || ("".equals(contextPath)))) {
+//			begin = contextPath.length();
+//		}
+//
+//		int end;
+//		if (uri.indexOf(";") != -1) {
+//			end = uri.indexOf(";");
+//		} else if (uri.indexOf("?") != -1) {
+//			end = uri.indexOf("?");
+//		} else {
+//			end = uri.length();
+//		}
+//
+//		String viewName = uri.substring(begin, end);
+//		if (viewName.indexOf(".") != -1) {
+//			viewName = viewName.substring(0, viewName.lastIndexOf("."));
+//		}
+//		if (viewName.lastIndexOf("/") != -1) {
+//			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
+//		}
+//		return viewName;
+//	}
 
 
 }
